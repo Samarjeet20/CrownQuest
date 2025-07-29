83e9e8e48edd5cd9b18eb91a62802775d006eed0
Samarjeet20 <samarjeetshinde20@gmail.com>
2025-07-29 12:10:27 +0530

    Socket server files set up done

--- server/src/socket/server.js
+++ server/src/socket/server.js
@@ -77,3 +77,71 @@ exports.SocketServer = (io) => {
       socket.emit("ongoing-game", game);
     }
     /* ---------------------------------- Check For Existing Games----------------------------------*/
+
+    sendAllGames(io);
+
+    /* game initiation handler calls BEGIN */
+    socket.on("create-game", createNewGame({ io, socket }));
+    socket.on("join-game", joinGameBoard({ io, socket }));
+    socket.on("random-play-guest", randomPlayGuest({ io, socket }));
+    socket.on("random-play-user", randomPlayUser({ io, socket }));
+    /* game initiation handler calls END */
+
+    /* Friend requests*/
+    socket.on("send-friend-request", sendFriendRequest({ io, socket }));
+    socket.on("respond-friend-request", respondFriendRequest({ io, socket }));
+    socket.on(
+      "friend-game-invite-send",
+      sendGameInviteToFriend({ io, socket })
+    );
+    //also emit "friend-game-invite-receive"
+    socket.on(
+      "friend-game-invite-accept",
+      acceptGameInviteToFriend({ io, socket })
+    );
+    socket.on(
+      "friend-game-invite-reject",
+      rejectGameInviteToFriend({ io, socket })
+    );
+
+    /* game event handler calls BEGIN */
+    /*  -- game chats handler calls BEGIN */
+    socket.on("send-msg", ({ gameId, msg }) => {
+      sendUserMessage({ gameId, msg, io, socket });
+    });
+
+    socket.on("opponent-video-ready", ({ peerId, gameId }) => {
+      userVideoHandler({ peerId, gameId, io });
+    });
+    /*  -- game chats handler calls END */
+
+    /*  -- game draw handler calls BEGIN */
+    socket.on("draw-accepted", ({ gameId }) => {
+      acceptDraw({ gameId, io, socket });
+    });
+
+    socket.on("draw-offered", ({ gameId }) => {
+      offerDraw({ gameId, io, socket });
+    });
+
+    socket.on("draw-rejected", ({ gameId }) => {
+      rejectDraw({ gameId, io, socket });
+    });
+    /*  -- game draw handler calls END */
+
+    socket.on("move-piece", onMovePiece({ io, socket }));
+    socket.on("quit-game", onQuitGame({ io, socket }));
+    /* game event handler calls END */
+
+    /* miscellaneous event handlers */
+    socket.on("leave-room", ({ roomId }) => {
+      socket.leave(roomId);
+      // sending the person count in the room
+      io.to(roomId).emit(
+        "head-count",
+        io.sockets.adapter.rooms.get(roomId).size
+      );
+    });
+    socket.on("exit-game-lobby", onLobbyExit({ io, socket }));
+  });
+};
