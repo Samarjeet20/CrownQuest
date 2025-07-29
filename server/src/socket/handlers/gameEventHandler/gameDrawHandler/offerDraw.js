c2ad53246f8b30c35702a2516f2dabd2646c7d9c
Samarjeet20 <samarjeetshinde20@gmail.com>
2025-07-29 14:16:20 +0530

    Added offer draw in game draw handler in game event handler

---
+++ server/src/socket/handlers/gameEventHandler/gameDrawHandler/offerDraw.js
@@ -0,0 +1,15 @@
+const {
+  getGameByID,
+} = require("../../../helpers/gameBoardHelpers/gamePlayManager");
+
+module.exports = ({ gameId, io, socket }) => {
+  const from = socket.id;
+  const game = getGameByID(gameId);
+  if (game === undefined) return;
+  //getting the player which is not me
+  //that means the opponent
+  const opponent = game.players.filter((player) => {
+    return player.socket.id != from;
+  });
+  socket.to(opponent[0].socket.id).emit("draw-offered");
+};
